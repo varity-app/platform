2d2cd3f9b77f2eabc6ca1be27c7dc213448ab98a
Callum Gundlach <gundlachcallum@gmail.com>
2021-05-22 12:06:49 -0400

    Update prefect flow for tickers ETL to add the short_name column

--- src/prefect_flows/fetch_tickers.py
+++ src/prefect_flows/fetch_tickers.py
@@ -4,10 +4,11 @@ Prefect flow for fetching the list of tickers from IEXCloud
 
 from typing import Dict, List, Tuple
 from os import environ as env
+import re
 import requests
+
 import psycopg2
 from psycopg2.extras import execute_values
-
 
 from prefect import task, Flow
 from prefect.storage import GCS
@@ -27,6 +28,7 @@ TICKER_COLUMNS = [
     "region",
     "currency",
     "cik",
+    "short_name",
 ]
 
 USERNAME = env.get("POSTGRES_USERNAME")
@@ -87,27 +89,67 @@ def parse_postgres_tuples(
     list of tuples with values in the order required by PostgreSQL
     """
 
-    new_tickers = [tuple([ticker[column] for column in columns]) for ticker in tickers]
+    new_tickers = [tuple(ticker[column] for column in columns) for ticker in tickers]
 
     return new_tickers
+
+
+@task
+def add_short_name(tickers: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    """Add the shortname column to the list of tickers"""
+
+    # Get list of all words in the tickers' names
+    names = [ticker["name"] for ticker in tickers]
+    names = " ".join(names)
+    words = names.split(" ")
+
+    # Regex expressions
+    name_rex = re.compile("")
+
+    words = [word for word in words if word.isalpha()]
+    words = [word for word in words if bool(name_rex.match(word))]
+
+    # Find unique words
+    word_counts = dict()
+    for word in words:
+        count = word_counts.get(word, 0) + 1
+        word_counts[word] = count
+
+    # Only select names that come up very infrequently
+    unique_words = [word for word in words if word_counts[word] <= 10]
+
+    # Set short_name field
+    for ticker in tickers:
+        name_words = ticker["name"].split(" ")
+        short_name = None
+
+        for word in name_words:
+            if word in unique_words:
+                short_name = word
+                break
+
+        ticker["short_name"] = short_name
+
+    return tickers
 
 
 def generate_query(columns: List[str]) -> str:
     """Generate a PostgreSQL query for inserting new rows into the Tickers column"""
 
-    query = f"""
+    sql_query = f"""
         INSERT INTO tickers ({", ".join(columns)})
         VALUES %s
-        ON CONFLICT (symbol, exchange) DO
-            UPDATE SET name = EXCLUDED.name, is_enabled = EXCLUDED.is_enabled, scraped_on = EXCLUDED.scraped_on;
+        ON CONFLICT (symbol) DO
+            UPDATE SET name = EXCLUDED.name, is_enabled = EXCLUDED.is_enabled, scraped_on = EXCLUDED.scraped_on,
+                short_name = EXCLUDED.short_name;
     """
 
-    return query
+    return sql_query
 
 
 @task
 def insert_query(
-    query: str,
+    sql_query: str,
     data: List[Tuple],
     username: str,
     database: str,
@@ -123,7 +165,7 @@ def insert_query(
     cur = conn.cursor()
 
     # Run query
-    execute_values(cur, query, data)
+    execute_values(cur, sql_query, data)
     conn.commit()
 
     # Close connection
@@ -143,20 +185,21 @@ with Flow("Get Tickers", run_config=run_config, storage=bucket) as flow:
     postgres_password = PrefectSecret("POSTGRES_PASSWORD")
 
     # Fetch and transform tickers
-    tickers = extract_tickers(iex_token=token)
-    tickers = transform_tickers(tickers)
-    tickers = parse_postgres_tuples(TICKER_COLUMNS, tickers)
+    ticker_list = extract_tickers(iex_token=token)
+    ticker_list = transform_tickers(ticker_list)
+    ticker_list = add_short_name(ticker_list)
+    ticker_list = parse_postgres_tuples(TICKER_COLUMNS, ticker_list)
 
     # Insert into postgres
     query = generate_query(TICKER_COLUMNS)
     insert_query(
-        query=query,
-        data=tickers,
+        sql_query=query,
+        data=ticker_list,
         username=USERNAME,
         password=postgres_password,
         database=DB,
         host=HOST,
     )
 
-# flow.run()
-flow.register(project_name=f"varity-{DEPLOYMENT}", labels=[])
+flow.run()
+# flow.register(project_name=f"varity-{DEPLOYMENT}", labels=[])
