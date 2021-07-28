9d1d38cae5e4c533fead1163f73d81d90d3072e9
Callum Gundlach <gundlachcallum@gmail.com>
2021-07-27 22:50:06 -0400

    New common protobuf definitions, common postgres method

---
+++ go/scraping/common/pg.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"os"
+
+	"github.com/go-pg/pg/v10"
+)
+
+// Initialize postgres
+func InitPostgres() *pg.DB {
+	hostname := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	database := os.Getenv("POSTGRES_DB")
+	username := os.Getenv("POSTGRES_USERNAME")
+	password := os.Getenv("POSTGRES_PASSWORD")
+
+	db := pg.Connect(&pg.Options{
+		Addr:     hostname + ":" + port,
+		User:     username,
+		Password: password,
+		Database: database,
+	})
+
+	return db
+}
--- go/scraping/reddit/go.mod
+++ go/scraping/reddit/go.mod
@@ -1,6 +1,6 @@
 module reddit-scraper
 
-go 1.13
+go 1.16
 
 replace github.com/VarityPlatform/scraping/protobuf/reddit => ../protobuf/reddit
 
